6f0e7aa2b82f1b5bc586073cd4eeb0a38206e366
Marcos <mbinoti@gmail.com>
2023-07-30 16:16:07 -0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -32,21 +32,21 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Home"),
         actions: <Widget>[
           Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Container(
               width: 36,
               height: 30,
               decoration: BoxDecoration(
                   color: Colors.grey[800],
                   borderRadius: BorderRadius.circular(10)),
-              child: Center(child: Text("0")),
+              child: const Center(child: Text("0")),
             ),
           )
         ],
       ),
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             children: <Widget>[
               Container(
@@ -54,7 +54,7 @@ class _HomePageState extends State<HomePage> {
                 height: 250,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
+                    image: const DecorationImage(
                         image: AssetImage('assets/images/one.jpg'),
                         fit: BoxFit.cover)),
                 child: Container(
@@ -68,71 +68,67 @@ class _HomePageState extends State<HomePage> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
-                      Text(
+                      const Text(
                         "Lifestyle Sale",
                         style: TextStyle(
                             color: Colors.white,
                             fontSize: 35,
                             fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                       Container(
                         height: 50,
-                        margin: EdgeInsets.symmetric(horizontal: 40),
+                        margin: const EdgeInsets.symmetric(horizontal: 40),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.white),
                         child: Center(
-                            child: Text(
-                          "Shop Now",
-                          style: TextStyle(
-                              color: Colors.grey[900],
-                              fontWeight: FontWeight.bold),
-                        )),
+                          child: Text(
+                            "Shop Now",
+                            style: TextStyle(
+                                color: Colors.grey[900],
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
                       ),
-                      SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                     ],
                   ),
                 ),
               ),
               const SizedBox(height: 20),
               Expanded(
-                  child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                children: _listItem
-                    .map((item) => Card(
-                          color: Colors.transparent,
-                          elevation: 0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                image: DecorationImage(
-                                    image: AssetImage(item),
-                                    fit: BoxFit.cover)),
-                            child: Transform.translate(
-                              offset: Offset(50, -50),
-                              child: Container(
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: 65, vertical: 63),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.white),
-                                child: Icon(
-                                  Icons.bookmark_border,
-                                  size: 15,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  children: _listItem
+                      .map((item) => Card(
+                            color: Colors.transparent,
+                            elevation: 0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: DecorationImage(
+                                      image: AssetImage(item),
+                                      fit: BoxFit.cover)),
+                              child: Transform.translate(
+                                offset: const Offset(50, -50),
+                                child: Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 65, vertical: 63),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.white),
+                                  child: const Icon(Icons.bookmark_border,
+                                      size: 15),
                                 ),
                               ),
                             ),
-                          ),
-                        ))
-                    .toList(),
-              ))
+                          ))
+                      .toList(),
+                ),
+              )
             ],
           ),
         ),
